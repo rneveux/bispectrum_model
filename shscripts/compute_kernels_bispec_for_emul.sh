8c25f4e0ba8273496e26bb1c04e93f85c553be52
rneveux <richard.neveux@gmail.com>
2024-05-02 22:36:55 +0100

    organisation repo

---
+++ shscripts/compute_kernels_bispec_for_emul.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=kernels_bispectrum
+#SBATCH --ntasks=1
+#SBATCH --time=50:00:00
+#SBATCH --mem=1G
+#SBATCH --output=out/%x.%j.out
+#SBATCH --error=err/%x.%j.err
+
+cd /home/rneveux/bispectrum/theory/
+
+module load anaconda
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/rneveux/cuba
+
+for l in 000 202
+do
+for i in `seq $1 $(($1 + 124))`
+do
+python kernels_bispectrum.py -cosmo $i  -redshift .38 -ells $l -directory /home/rneveux/kernels_EFT/bk/z0.38/Omfid.317/kernels/ -Omfid .317 -k_list /home/rneveux/kernels_EFT/bk/z0.38/Omfid.317/k_data.txt
+done
+done
